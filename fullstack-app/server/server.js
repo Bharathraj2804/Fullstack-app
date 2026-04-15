c211504ef3c1876041342469eb1d16c7d8ad990f
Bharathraj2804 <ch.bharathraj@gmail.com>
2026-04-15 12:23:57 +0530

    fix home route

--- fullstack-app/server/server.js
+++ fullstack-app/server/server.js
@@ -7,6 +7,11 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use(express.static(__dirname));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
 // MongoDB connect
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("DB Connected"))
@@ -63,3 +68,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
+
